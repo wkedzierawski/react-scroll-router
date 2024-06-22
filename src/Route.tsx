8acc6b94067f23d51dc4dc4a8ea739d56de70a5b
wkedzierawski <wiktorkedzierawski@gmail.com>
2024-06-22 18:05:18 +0200

    containerStyle for ScrollRoute

--- src/Route.tsx
+++ src/Route.tsx
@@ -7,71 +7,80 @@ import { useScrollRouter } from "./hooks";
 type Props = {
   route: string;
   children: JSX.Element;
+  containerStyle?: React.CSSProperties;
 };
 
 const safetyMargin = -10;
 
-export const ScrollRoute = memo(({ children, route }: Props) => {
-  const ref = useRef<HTMLDivElement | null>(null);
+export const ScrollRoute = memo(
+  ({ children, route, containerStyle }: Props) => {
+    const ref = useRef<HTMLDivElement | null>(null);
 
-  const { replaceHistory } = useScrollRouter();
+    const { replaceHistory } = useScrollRouter();
 
-  const onPageScroll = useCallback(() => {
-    if (!ref.current) {
-      return;
-    }
-
-    const dimensions = ref.current.getClientRects()?.[0];
-
-    if (!dimensions) {
-      return;
-    }
-
-    const offsetFromTop = -dimensions.y;
-
-    const currentSection = isBetween(
-      offsetFromTop,
-      0 + safetyMargin,
-      dimensions.height + safetyMargin
-    );
-
-    currentSection && replaceHistory(route);
-  }, [replaceHistory, route]);
-
-  const scrollActiveRouteIntoView = useCallback(() => {
-    if (window.location.pathname !== route) {
-      return;
-    }
-
-    ref.current?.scrollIntoView();
-  }, [route]);
-
-  useEffect(scrollActiveRouteIntoView, [scrollActiveRouteIntoView]);
-
-  useEffect(() => {
-    window.addEventListener("scroll", onPageScroll);
-
-    return () => {
-      window.removeEventListener("scroll", onPageScroll);
-    };
-  }, [onPageScroll, scrollActiveRouteIntoView]);
-
-  useEffect(() => {
-    const removeListener = ScrollRouter.addListener(
-      ScrollEvent.SCROLL_TO_ROUTE,
-      ({ detail: payload }) => {
-        if (payload.route === route) {
-          const scrollOptions = payload.scrollOptions || { behavior: "smooth" };
-          ref.current?.scrollIntoView(scrollOptions);
-        }
+    const onPageScroll = useCallback(() => {
+      if (!ref.current) {
+        return;
       }
+
+      const dimensions = ref.current.getClientRects()?.[0];
+
+      if (!dimensions) {
+        return;
+      }
+
+      const offsetFromTop = -dimensions.y;
+
+      const currentSection = isBetween(
+        offsetFromTop,
+        0 + safetyMargin,
+        dimensions.height + safetyMargin
+      );
+
+      currentSection && replaceHistory(route);
+    }, [replaceHistory, route]);
+
+    const scrollActiveRouteIntoView = useCallback(() => {
+      if (window.location.pathname !== route) {
+        return;
+      }
+
+      ref.current?.scrollIntoView();
+    }, [route]);
+
+    useEffect(scrollActiveRouteIntoView, [scrollActiveRouteIntoView]);
+
+    useEffect(() => {
+      window.addEventListener("scroll", onPageScroll);
+
+      return () => {
+        window.removeEventListener("scroll", onPageScroll);
+      };
+    }, [onPageScroll, scrollActiveRouteIntoView]);
+
+    useEffect(() => {
+      const removeListener = ScrollRouter.addListener(
+        ScrollEvent.SCROLL_TO_ROUTE,
+        ({ detail: payload }) => {
+          if (payload.route === route) {
+            const scrollOptions = payload.scrollOptions || {
+              behavior: "smooth",
+            };
+            ref.current?.scrollIntoView(scrollOptions);
+          }
+        }
+      );
+
+      return removeListener;
+    }, [route]);
+
+    return (
+      <Container style={containerStyle} ref={ref}>
+        {children}
+      </Container>
     );
-
-    return removeListener;
-  }, [route]);
-
-  return <Container ref={ref}>{children}</Container>;
-});
+  }
+);
 
 const Container = styled.div`
   width: 100%;
